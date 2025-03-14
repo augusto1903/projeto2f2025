3c0721486b1402c056dc8ac7c06caea8fa820a79
auguso1903 <rm95644@estudante.fieb.edu.br>
2025-03-14 09:38:37 -0300

    projeto2f

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import [ Component ] from "react"; 
+
+class App extends Cómponent {
+
+  render() {
+
+    return (
+      <div>
+        <h3>Aula React Vite</h3>
+        <p>Retomada de Conteúdo</p>
+      </div>
+    )
+  }
+}
+
+export default App
+ 
